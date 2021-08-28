dac8b4a9cdbc94a937bcf2e266eed51aa8daf437
Dan Cruzat <thecruzat@gmail.com>
2021-08-28 13:02:42 -0700

    Add socials meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Dan Cruzat is a front end engineer with over 20 years\' experience in digital, currently focused on React and Vue. Located in Portland OR.' },
+      { name: 'keywords', content: 'front end, developer, engineer, wordpress, vue, react, html, css, javascript, portland, oregon' },
+      { name: 'author', content: 'Dan Cruzat' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@_cruzat' },
+      { name: 'twitter:creator', content: '@_cruzat' },
+      { name: 'twitter:title', content: 'Built by the Cruzat : Web Portfolio of Dan Cruzat' },
+      { name: 'twitter:description', content: 'Dan Cruzat is a front end engineer with over 20 years\' experience in digital, currently focused on React and Vue. Located in Portland OR.' },
+      { name: 'twitter:image', content: '/builtbycruzat.png' },
+      { property: 'og:title', content: 'Built by the Cruzat : Web Portfolio of Dan Cruzat' },
+      { property: 'og:description', content: 'Dan Cruzat is a front end engineer with over 20 years\' experience in digital, currently focused on React and Vue. Located in Portland OR.' },
+      { property: 'og:image', content: '/builtbycruzat.png' },
       // { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
