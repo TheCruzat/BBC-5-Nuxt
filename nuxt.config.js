e802dc35261dfc3b91f54f272c8a41cfd6f92c91
Dan Cruzat <thecruzat@gmail.com>
2021-08-29 13:31:10 -0700

    Add GFonts, GA Tags, Fontawesome refs

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
       // { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' }
     ]
   },
 
@@ -35,14 +36,14 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/google-fonts'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -51,5 +52,19 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
+  },
+
+  router: {
+    base: '/',
+    routeNameSplitter: '/'
+  },
+
+  googleFonts: {
+    families: {
+      'Bree+Serif': true,
+    },
+    display: 'swap',
+    prefetch: true,
   }
 }
