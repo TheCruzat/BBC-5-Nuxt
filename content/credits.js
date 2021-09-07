60f55fb4f2258f0b1b324bad188462b9f11bca97
Dan Cruzat <thecruzat@gmail.com>
2021-09-06 21:11:46 -0700

    Create data object for Projects + Positions

---
+++ content/credits.js
@@ -0,0 +1,111 @@
+const credits = [
+  /*
+  {
+    company: "",
+    position: "",
+    time: "",
+    tech: "",
+    deets: [
+      "",
+      "",
+      ""
+    ]
+  },
+  */
+  {
+    company: "Emerge Interactive",
+    position: "Lead Developer, Theme / UI Developer",
+    time: "2021",
+    tech: "HTML, WordPress, SCSS, Docker, ACF",
+    deets: [
+      "Project lead on KIT build, layouts pre-theme",
+      "Layout development for custom post types and global frame",
+      "Team Support",
+      "QA / bugfixing / browser testing",
+    ]
+  },
+  {
+    company: "ShirtSpace",
+    position: "Senior Web Developer",
+    time: "2021",
+    tech: "Slim, SCSS, JS/ES6, JQuery, Lighthouse",
+    deets: [
+      "Audit global structure in testing for web core vitals optimization, esp CLS & LCP",
+      "Custom UI enhancements using forward-facing ES6",
+      "UX strategy, userflow and engagement",
+      "QA / bugfixing / browser testing (incl IE legacy)",
+    ]
+  },
+  {
+    company: "BASIC",
+    position: "UI / Front End Developer",
+    time: "2020",
+    tech: "React (Next, Gatsby), StorybookJS, SCSS",
+    deets: [
+      "layout / interactive development for food delivery app",
+      "layout / interactive development for brand merch store",
+      "element work for library",
+      "QA / bugfixing"
+    ]
+  },
+  {
+    company: "creative FEW",
+    position: "Front End Developer, Support Developer",
+    time: "2017-2020",
+    tech: "React (Next, Gatsby), Angular, WordPress, ACF, HTML, PHP, SCSS, Gulp, Docker",
+    deets: [
+      "full stack dev in WordPress",
+      "layout / component / front end dev in React, Angular",
+      "static HTML development",
+      "QA / bugfixing"
+    ]
+  },
+  {
+    company: "E+C",
+    position: "Senior Developer, UX Strategist, Colleague",
+    time: "2013-2020",
+    tech: "WordPress full stack, ACF, PHP, SCSS, Gulp, Docker",
+    deets: [
+      "worked with clients to define and develop solutions",
+      "developed reuseable SPA's for small biz requirements",
+      "engaged with agencies and PMs to assist with workload"
+    ]
+  },
+  {
+    company: "Industrial Commercial Electric",
+    position: "Lead Developer, UX Support",
+    time: "2018-2019",
+    tech: "WordPress full stack, PHP, ACF, SCSS, JQuery, Gulp",
+    deets: [
+      "custom responsive theme dev w/ 7 layouts",
+      "custom employee portal dev incl login logic",
+      "custom timecard reporting system",
+      "QA / bugfixing / browser testing"
+    ]
+  },
+  {
+    company: "Modern Adventure",
+    position: "WordPress Specialist, Team Support",
+    time: "2018",
+    tech: "Trellis, Vagrant, SASS, JQuery, PHP, CMB2",
+    deets: [
+      "paired w/ inhouse dev to meet org needs",
+      "client portal minisite layout templates",
+      "built blog/magazine layout templates",
+      "bugfixes and UI assistance"
+    ]
+  },
+  {
+    company: "+Citizen (now EY)",
+    position: "Front End Developer, CSS Specialist, QA Testing",
+    time: "2015-2017",
+    tech: "AngularJS, SCSS",
+    deets: [
+      "worked on team of 10-15 building enterprise customer portal",
+      "layout / interactive / component development",
+      "QA Tester / UI problemsolver"
+    ]
+  },
+]
+
+export default credits;
