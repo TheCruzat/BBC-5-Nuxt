e15fb37801cafee9d9774c94e2a5d48cb15e473c
Dan Cruzat <thecruzat@gmail.com>
2025-10-20 11:56:32 -0700

    Revise blurbiage

--- content/resSummary.vue
+++ content/resSummary.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="summary">
-    <p>Seasoned Front End Developer / UI Engineer working with teams, outfits and agencies. Experienced remote resource based in Portland OR, available for tasks domestic and distant. I am strongest in layout, interactive and styling code, a good fit for a team needing a solid FED with a focus on CSS, polish and presentation.</p>
-    <p>Let's make things as pretty as we can.</p>
+    <p>Seasoned Front End Developer / UI Engineer available to assist organizations large and small. Experienced remote resource based in Portland OR, available for tasks domestic and distant. Solid FED expertise across systems and platforms, they all use JavaScript and CSS.</p>
+    <p>Currently focused in the many interactions between React and WordPress, random bits in Vue.</p>
     <!-- <p>Currently working in WordPress, noodling around in Vue and React.</p> -->
   </div>
 </template>
