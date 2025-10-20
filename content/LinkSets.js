92cf755f6fb88b33383265411cbdeb3537f434a3
Dan Cruzat <thecruzat@gmail.com>
2025-10-20 11:56:48 -0700

    Revise creds + links

--- content/LinkSets.js
+++ content/LinkSets.js
@@ -1,8 +1,9 @@
 export const
-  resumeFile = '/Dan-Cruzat-resume-2025.pdf',
+  resumeFile = '/Dan-Cruzat-resume-1025.pdf',
   resumeURL = '/resume',
   rootURL = 'https://thecruzat.com',
   portURL = 'https://builtby.thecruzat.com',
+  codeURL = 'https://thecruzat.com/code-samples',
   blogURL = 'https://coffeewith.thecruzat.com',
   liURL = 'https://www.linkedin.com/in/dancruzat/',
   ghURL = 'https://github.com/thecruzat',
@@ -13,29 +14,39 @@ export const LinkSets = [
   {
     header: "client work",
     links: [
+      // {
+      //     title: "",
+      //     desc: "",
+      //     url: "",
+      // },
+      {
+        title: "Cole Parker Randall Foundation",
+        desc: "WordPress, Tailwind, ES6",
+        url: "//smilesforsma.org"
+      },
       {
         title: "Miriam Ziven",
-        desc: "WordPress, Tailwind, JS",
+        desc: "WordPress, Tailwind, ES6",
         url: "//miriamziven.com",
       },
       {
         title: "BeLearning Center",
-        desc: "WordPress, NPM, SASS, JS",
+        desc: "WordPress, npm, SASS, ES6",
         url: "//belearning.center",
       },
       {
         title: "Ells.Work",
-        desc: "Craft CMS, Gulp, SASS, JS",
+        desc: "Craft CMS, Gulp, SASS, ES6",
         url: "//ells.work",
       },
       {
         title: "MIT School of Humanities, Arts and Social Sciences",
-        desc: "WordPress, Gulp, SASS, JS",
+        desc: "WordPress, API, Gulp, SASS, ES6",
         url: "//shass.mit.edu",
       },
       {
         title: "MIT Institutional Research",
-        desc: "WordPress, Gulp, SASS, JS",
+        desc: "WordPress, Gulp, SASS, ES6",
         url: "//ir.mit.edu",
       },
       {
@@ -73,11 +84,6 @@ export const LinkSets = [
         desc: "WordPress, LESS, JQuery",
         url: "//synergywellness.org",
       },
-      // {
-      //     title: "",
-      //     desc: "",
-      //     url: "",
-      // },
     ]
   },
   {
@@ -85,13 +91,13 @@ export const LinkSets = [
     links: [
       {
         title: "More About Me",
-        desc: "NextJS, Styled Components",
-        url: "//thecruzat.com",
+        desc: "React/NextJS, API, Tailwind, ES6",
+        url: rootURL,
       },
       {
         title: "Coffee with the Cruzat",
-        desc: "WordPress, Gulp, SCSS, JQuery",
-        url: "//coffeewith.thecruzat.com",
+        desc: "React Blocks, Tailwind, ES6",
+        url: blogURL,
       },
       {
         title: "ARX by P1RK5",
@@ -119,6 +125,12 @@ export const GoodiesLinks = [
     label: "blog",
     url: blogURL,
     icon: "fa-coffee",
+  },
+  {
+    title: "Review Dan Cruzat's Code",
+    label: "code",
+    url: codeURL,
+    icon: "fa-code",
   },
   {
     title: "Email Dan Cruzat",
